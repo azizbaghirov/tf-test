90f266c3bb179e8435d2b514eda191850c33cd2b
Aziz-Baghirov-Digirella <aziz.baghirov@digirella.az>
2024-05-13 12:12:44 +0400

    Commit 4

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@
 # provider "aws" {
 #   #access_key = var.aws_access_key
 #   #secret_key = var.aws_secret_key
-#   region     = var.region
+   region     = var.region
 # }
 
 # # Add .gitignore file in this directory with the terraform.tfvars
